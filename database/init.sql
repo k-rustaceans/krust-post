d2a4f3aba1df767b35264c6654e94881184a2bcd
jerok <jerok.kim@gmail.com>
2023-07-25 21:50:07 +0900

    init sql insert dummy data

--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE IF NOT EXISTS user (
+﻿CREATE TABLE IF NOT EXISTS member (
 	id        SERIAL PRIMARY KEY,
 	email     VARCHAR(64) NOT NULL UNIQUE,
 	password  varchar(64) NOT NULL,
@@ -9,11 +9,11 @@
 
 CREATE TABLE IF NOT EXISTS post (
 	id         serial PRIMARY KEY,
-	user_id    integer     DEFAULT NULL,
+	member_id  integer     DEFAULT NULL,
 	create_dt  timestamptz DEFAULT NULL,
 	update_dt  timestamptz DEFAULT NULL,
 	is_deleted boolean     DEFAULT FALSE,
-	CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user (id)
+	CONSTRAINT fk_member FOREIGN KEY (member_id) REFERENCES member (id)
 )
 ;
 
@@ -28,9 +28,9 @@ CREATE TABLE IF NOT EXISTS post_content (
 ;
 
 CREATE TABLE IF NOT EXISTS post_like (
-	user_id integer DEFAULT NULL,
-	post_id integer DEFAULT NULL,
-	CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user (id),
+	member_id integer DEFAULT NULL,
+	post_id   integer DEFAULT NULL,
+	CONSTRAINT fk_member FOREIGN KEY (member_id) REFERENCES member (id),
 	CONSTRAINT fk_post FOREIGN KEY (post_id) REFERENCES post (id)
 )
 ;
@@ -38,19 +38,27 @@ CREATE TABLE IF NOT EXISTS post_like (
 CREATE TABLE IF NOT EXISTS comment (
 	id        serial PRIMARY KEY,
 	post_id   integer     DEFAULT NULL,
-	user_id   integer     DEFAULT NULL,
+	member_id integer     DEFAULT NULL,
 	content   text        DEFAULT NULL,
 	create_dt timestamptz DEFAULT NULL,
 	update_dt timestamptz DEFAULT NULL,
 	CONSTRAINT fk_post FOREIGN KEY (post_id) REFERENCES post (id),
-	CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user (id)
+	CONSTRAINT fk_member FOREIGN KEY (member_id) REFERENCES member (id)
 )
 ;
 
 CREATE TABLE IF NOT EXISTS comment_like (
-	user_id    integer DEFAULT NULL,
+	member_id  integer DEFAULT NULL,
 	comment_id integer DEFAULT NULL,
-	CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user (id),
+	CONSTRAINT fk_member FOREIGN KEY (member_id) REFERENCES member (id),
 	CONSTRAINT fk_comment FOREIGN KEY (comment_id) REFERENCES comment (id)
 )
 ;
+
+INSERT INTO
+	member(email, password)
+VALUES
+	('jerok.kim@gmail.com', 'jerok')
+;
+
+-- TODO: insert post, post_content, post_like, comment, comment_like
