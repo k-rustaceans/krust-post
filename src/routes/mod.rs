8237204ad690bd08c0248449c875a350869276f9
jerok <jerok.kim@gmail.com>
2023-07-25 16:32:22 +0900

    cors layer added

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -6,14 +6,22 @@ use axum::{
     body::Body,
     routing::{
         get,
-        post
-    }
+        post,
+    },
 };
+use axum::http::Method;
+use tower_http::cors::{Any, CorsLayer};
 use crate::routes::create_post::create_post;
 use crate::routes::home::index;
 
 pub fn create_routes() -> Router<(), Body> {
+    // used tower-http middleware for cors
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
     Router::new()
         .route("/", get(index))
         .route("/post", post(create_post))
+        .layer(cors)
 }
