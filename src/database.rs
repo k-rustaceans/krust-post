352608b6fc28f5f075065291b15dec8a8f478dbe
Migo <migo@Migos-MacBook-Pro.local>
2023-08-18 19:12:49 +0900

    [INFRA] Pulsar client set up & docker compose

--- src/database.rs
+++ src/database.rs
@@ -1,8 +1,13 @@
 use std::env;
+use std::ops::{Deref, DerefMut};
 use std::sync::OnceLock;
 use std::{mem, sync::Arc};
 
 use event_driven_library::responses::BaseError;
+
+use pulsar::message::proto::command_subscribe::SubType;
+use pulsar::{message::proto, producer, Pulsar};
+use pulsar::{Consumer, DeserializeMessage, Producer, TokioExecutor};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{postgres::PgPool, Postgres, Transaction};
 
@@ -94,4 +99,188 @@ pub async fn connection_pool() -> &'static PgPool {
 		Some(pool) => pool,
 	};
 	p
+}
+
+pub struct QueuePubExecutor {
+	pub(crate) producer: Producer<TokioExecutor>,
+}
+
+impl QueuePubExecutor {
+	pub async fn new() -> Self {
+		Self {
+			producer: {
+				dotenv::dotenv().ok();
+				let url = &env::var("QUEUE_URL").expect("QUEUE_URL must be set");
+				let pulsar: Pulsar<_> = Pulsar::builder(url, TokioExecutor).build().await.unwrap();
+
+				pulsar
+					.producer()
+					.with_topic("non-persistent://public/default/test")
+					.with_name("my producer")
+					.with_options(producer::ProducerOptions {
+						schema: Some(proto::Schema {
+							r#type: proto::schema::Type::String as i32,
+							..Default::default()
+						}),
+						..Default::default()
+					})
+					.build()
+					.await
+					.unwrap()
+			},
+		}
+	}
+}
+
+impl From<QueuePubExecutor> for Arc<RwLock<QueuePubExecutor>> {
+	fn from(value: QueuePubExecutor) -> Self {
+		Arc::new(RwLock::new(value))
+	}
+}
+
+impl Deref for QueuePubExecutor {
+	type Target = Producer<TokioExecutor>;
+	fn deref(&self) -> &Self::Target {
+		&self.producer
+	}
+}
+impl DerefMut for QueuePubExecutor {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		&mut self.producer
+	}
+}
+
+pub struct QueueConExecutor<T: DeserializeMessage> {
+	pub(crate) consumer: Consumer<T, TokioExecutor>,
+}
+
+impl<T: DeserializeMessage + 'static> QueueConExecutor<T> {
+	pub async fn new(
+		topic: &str,
+		subscription: &str,
+	) -> Self {
+		dotenv::dotenv().ok();
+		let url = &env::var("QUEUE_URL").expect("QUEUE_URL must be set");
+		let pulsar: Pulsar<_> = Pulsar::builder(url, TokioExecutor).build().await.unwrap();
+		Self {
+			consumer: pulsar
+				.consumer()
+				.with_topic(topic)
+				.with_consumer_name("test_consumer")
+				.with_subscription_type(SubType::Exclusive)
+				.with_subscription(subscription)
+				.build()
+				.await
+				.unwrap(),
+		}
+	}
+}
+
+impl<T: DeserializeMessage + 'static> Deref for QueueConExecutor<T> {
+	type Target = Consumer<T, TokioExecutor>;
+	fn deref(&self) -> &Self::Target {
+		&self.consumer
+	}
+}
+impl<T: DeserializeMessage + 'static> DerefMut for QueueConExecutor<T> {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		&mut self.consumer
+	}
+}
+
+#[cfg(test)]
+mod test {
+
+	use futures::TryStreamExt;
+	use pulsar::{producer, Error as PulsarError, SerializeMessage};
+	use pulsar::{DeserializeMessage, Payload};
+	use serde::{Deserialize, Serialize};
+
+	use super::QueuePubExecutor;
+
+	use super::QueueConExecutor;
+
+	#[derive(Serialize, Deserialize)]
+	struct TestData {
+		data: String,
+	}
+	impl SerializeMessage for TestData {
+		fn serialize_message(input: Self) -> Result<producer::Message, PulsarError> {
+			let payload =
+				serde_json::to_vec(&input).map_err(|e| PulsarError::Custom(e.to_string()))?;
+			Ok(producer::Message {
+				payload,
+				..Default::default()
+			})
+		}
+	}
+
+	async fn set_up<T: DeserializeMessage + 'static>(
+		topic: &str,
+		subscription: &str,
+	) -> Result<(), PulsarError> {
+		let mut consumer: QueueConExecutor<T> = QueueConExecutor::new(topic, subscription).await;
+		println!("dsds");
+
+		while let Some(msg) = consumer.try_next().await? {
+			println!("Some value! ");
+			consumer.ack(&msg).await?;
+		}
+		Ok(())
+	}
+
+	#[tokio::test]
+	async fn test_publish() {
+		let mut producer = QueuePubExecutor::new().await;
+		match producer
+			.send(TestData {
+				data: "data".to_string(),
+			})
+			.await
+			.unwrap()
+			.await
+		{
+			Ok(val) => println!("{:?}", val),
+			Err(err) => panic!("{}", err),
+		}
+	}
+
+	impl DeserializeMessage for TestData {
+		type Output = Result<TestData, serde_json::Error>;
+
+		fn deserialize_message(payload: &Payload) -> Self::Output {
+			serde_json::from_slice(&payload.data)
+		}
+	}
+
+	#[tokio::test]
+	#[ignore]
+	//TODO get this test passed!
+	async fn test_round_trip() {
+		'_given: {
+			let topic = "test";
+			let subscription = "test_subscription";
+			set_up::<TestData>(topic, subscription).await.unwrap();
+
+			'_when: {
+				let mut producer = QueuePubExecutor::new().await;
+				producer
+					.send(TestData {
+						data: "TestDataForRoundTrip".to_string(),
+					})
+					.await
+					.unwrap();
+
+				let mut consumer: QueueConExecutor<TestData> =
+					QueueConExecutor::new(topic, subscription).await;
+
+				if let Some(msg) = consumer.try_next().await.unwrap() {
+					let data = msg.deserialize().unwrap();
+					assert_eq!(data.data, "TestDataForRoundTrip".to_string());
+				} else {
+					panic!("Test Failed!")
+				}
+			}
+		}
+	}
 }
