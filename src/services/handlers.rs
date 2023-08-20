d8f361ce9299de170b996e4b5244aaea7a6d2896
Migorithm <saka90030@gmail.com>
2023-08-20 23:35:59 +0900

    [FEATURE] Broker implemented

--- src/services/handlers.rs
+++ src/services/handlers.rs
@@ -1,15 +1,27 @@
+use std::collections::hash_map::Entry;
+
 use axum::extract::ws::{Message, WebSocket};
-use futures_util::{SinkExt, StreamExt};
-use tokio::sync::broadcast::{self, Sender};
+
+use futures_util::{
+	stream::{SplitSink, SplitStream},
+	SinkExt, StreamExt,
+};
+
+use tokio::{
+	sync::broadcast::{self},
+	task::JoinHandle,
+};
 
 use crate::domain::thread::{schemas::ClientMessage, Chatters, ThreadStateWrapper};
+
+use super::response::ServiceError;
 
 pub(crate) struct ThreadHandler;
 impl ThreadHandler {
 	/// This function deals with a single websocket connection, i.e., a single
 	/// connected client / user, for which we will spawn two independent tasks (for
 	/// receiving / sending chat messages).
-	async fn handle_socket(
+	async fn run_socket_broker(
 		stream: WebSocket,
 		state: ThreadStateWrapper,
 	) {
@@ -17,35 +29,72 @@ impl ThreadHandler {
 
 		let mut chatters: Option<Chatters> = None;
 		while let Some(Ok(message)) = receiver.next().await {
-			if let Ok(ClientMessage::JoinMainThread { post_id, .. }) = message.try_into() {
+			if let Ok(ClientMessage::JoinChat { post_id, .. }) = message.try_into() {
 				chatters = Some(ThreadHandler::get_or_create_thread(post_id, state.clone()).await);
 			} else {
 				let _ = sender
 					.send(String::from("Wrong input was given").into())
 					.await;
+				return;
 			}
 		}
 		let chatters = chatters.take().unwrap();
 
 		// Receiver from given 'Chatters' object that belongs only to the single instance
-		let mut rx = chatters.subscribe();
 
-		// TODO Send the join message to queue
+		let mut send_task =
+			ThreadHandler::_send_messages_from_chatroom_to_this_user(chatters.clone(), sender);
 
-		// TODO take message from either queue(pulsar) or chatters in the given application instance
+		let mut recv_task = ThreadHandler::_receive_messages_from_this_user(receiver);
+		tokio::select! {
+			_ = (&mut send_task) => recv_task.abort(),
+			_ = (&mut recv_task) => send_task.abort(),
+		};
+	}
+
+	fn _send_messages_from_chatroom_to_this_user(
+		chatters: Chatters,
+		mut sender: SplitSink<WebSocket, Message>,
+	) -> JoinHandle<()> {
+		tokio::spawn(async move {
+			while let Ok(msg) = chatters.subscribe().recv().await {
+				if sender.send(Message::Text(msg)).await.is_err() {
+					break;
+				}
+			}
+		})
+	}
+	fn _receive_messages_from_this_user(mut receiver: SplitStream<WebSocket>) -> JoinHandle<()> {
+		tokio::spawn(async move {
+			'recv_loop: loop {
+				if let Some(Ok(message)) = receiver.next().await {
+					match std::convert::TryInto::<ClientMessage>::try_into(message) {
+						Ok(client_message) => {
+							// TODO send client message to the topic
+						}
+						Err(ServiceError::UserCloseConnection) => {
+							println!("User closed the connection!");
+							// TODO send user_quit message to the topic
+							break 'recv_loop;
+						}
+						_ => break 'recv_loop,
+					}
+				}
+			}
+		})
 	}
 
 	async fn get_or_create_thread(
-		post_id: i64,
+		id: i64,
 		state: ThreadStateWrapper,
 	) -> Chatters {
-		let (tx, _rx) = broadcast::channel(100);
-
-		state
-			.write()
-			.await
-			.entry(post_id.into())
-			.or_insert(tx.clone().into());
-		tx.into()
+		match state.write().await.entry(id.into()) {
+			Entry::Occupied(occupied_entry) => occupied_entry.get().clone(),
+			Entry::Vacant(vacant_entry) => {
+				let (tx, _rx) = broadcast::channel(100);
+				vacant_entry.insert(tx.clone().into());
+				tx.into()
+			}
+		}
 	}
 }
